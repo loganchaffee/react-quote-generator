cfb0117d7a54307390daedaa3e20a6137a23a96a
Logan Chaffee <loganschaffee@gmail.com>
2021-08-30 19:22:22 -0700

    Add rel='noreferrer'

--- src/TweetButton.js
+++ src/TweetButton.js
@@ -3,6 +3,7 @@ const TweetButton = (props) => {
         <a
             className='tweet-button'
             target='_blank'
+            rel="noreferrer"
             href={`https://twitter.com/intent/tweet?text=${props.quoteText}  -${props.author}`}
         >
             Tweet
